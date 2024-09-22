326deccb11715e1d9417e6a81ef89fce66e907c6
dvolper <github@david.vollmers.org>
2024-09-22 18:11:47 +0200

    chore: fix lint issues

--- @cele/core/src/lib/jsx.ts
+++ @cele/core/src/lib/jsx.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-namespace,@typescript-eslint/no-unused-vars
 declare namespace JSX {
   interface IntrinsicElements {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     [elemName: string]: any;
   }
 }
