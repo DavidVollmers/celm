76d97c3b0398af318f64f39ba8de8ed4cf28a0a0
dvolper <github@david.vollmers.org>
2024-09-23 15:09:19 +0200

    chore: add button styles for design token concept

--- examples/lib/src/lib/button/button.styles.ts
+++ examples/lib/src/lib/button/button.styles.ts
@@ -4,4 +4,18 @@ export const buttonStyles = css`
   :host {
     display: inline-flex;
   }
+
+  button {
+    border: none;
+    background-color: blue;
+    margin: 0;
+    padding: 0.5rem 1rem;
+    border-radius: 0.25rem;
+    color: white;
+    transition: 0.1s linear background-color;
+  }
+
+  button:hover {
+    background-color: darkblue;
+  }
 `;
