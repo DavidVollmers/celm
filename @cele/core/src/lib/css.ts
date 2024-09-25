84bdd29db854c5654c35e38a2ebd54faf4337c97
dvolper <github@david.vollmers.org>
2024-09-25 14:28:24 +0200

    feat(core)!: return CSSStyleSheet directly from css template instead of function

--- @cele/core/src/lib/css.ts
+++ @cele/core/src/lib/css.ts
@@ -1,5 +1,3 @@
-import { CElementStylesFunction } from './celement-definition';
-import { CElement } from './celement';
 import { DesignToken } from './design-token';
 import { DesignTokenMap } from './design-token-map';
 
@@ -10,30 +8,28 @@ export type CSSTemplateValue =
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   | DesignTokenMap<any>;
 
-export const css = function <T extends CElement>(
+//TODO allow using "this" inside css templates (return CSSStyleSheetFunction or CSSStyleSheet)
+export const css = function (
   strings: TemplateStringsArray,
   ...values: CSSTemplateValue[]
-): CElementStylesFunction<T> {
-  return function (this: T): CSSStyleSheet {
-    let content = '';
-    for (let i = 0; i < strings.length; i++) {
-      content += strings[i];
-      if (i < values.length) {
-        const value = values[i];
-        if (typeof value === 'string' || typeof value === 'number')
-          content += value.toString();
-        else if (value instanceof DesignToken)
-          content += `var(${value.varName})`;
-        else {
-          for (const property in value) {
-            const token = value[property];
-            content += `${token.propertyName}: var(${token.varName});`;
-          }
+): CSSStyleSheet {
+  let content = '';
+  for (let i = 0; i < strings.length; i++) {
+    content += strings[i];
+    if (i < values.length) {
+      const value = values[i];
+      if (typeof value === 'string' || typeof value === 'number')
+        content += value.toString();
+      else if (value instanceof DesignToken) content += `var(${value.varName})`;
+      else {
+        for (const property in value) {
+          const token = value[property];
+          content += `${token.propertyName}: var(${token.varName});`;
         }
       }
     }
-    const sheet = new CSSStyleSheet();
-    sheet.replaceSync(content);
-    return sheet;
-  };
+  }
+  const sheet = new CSSStyleSheet();
+  sheet.replaceSync(content);
+  return sheet;
 };
