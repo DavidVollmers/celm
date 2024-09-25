b0b99898277d9cb3a0485164282c68a31d9d0aba
dvolper <github@david.vollmers.org>
2024-09-25 14:30:06 +0200

    chore: update example app

--- examples/app/src/app/app.template.tsx
+++ examples/app/src/app/app.template.tsx
@@ -1,13 +1,9 @@
 import { App } from './app';
-import { Button, theme } from 'examples-lib';
+import { Button } from 'examples-lib';
 
 export const appTemplate = function (this: App) {
-  function changeDesignToken() {
-    theme.colors.primary['500'].setValue('#00ff00');
-  }
-
   return (
-    <Button onClick={changeDesignToken}>
+    <Button onClick={() => alert('Hello World!')}>
       Hello <b>World</b>!
     </Button>
   );
