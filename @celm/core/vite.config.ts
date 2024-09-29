e7af09b0ce7a35b8450ec8fb79d710e76e06a9f2
dvolper <github@david.vollmers.org>
2024-09-29 10:45:27 +0200

    chore: add license files to packages

--- @celm/core/vite.config.ts
+++ @celm/core/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 
   plugins: [
     nxViteTsPaths(),
-    nxCopyAssetsPlugin(['*.md']),
+    nxCopyAssetsPlugin(['*.md', '*.txt']),
     dts({
       entryRoot: 'src',
       tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
